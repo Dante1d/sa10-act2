bfdbabd39b636ab30f710cdbae73d7e15140abea
Dante Donaldson <ddonaldsonprime@gmail.com>
2024-02-27 16:06:46 -0600

    Made classes for different methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,6 +7,37 @@ class GildedRose
     @quality = quality
   end
 
+  def normal_tick
+    @item = Normal.new(quality, days_remaining)
+    item.tick
+  end
+
+  def brie_tick
+    @item = Brie.new(quality, days_remaining)
+    item.tick
+  end
+
+  def sulfuras_tick
+    @item = Sulfuras.new(quality, days_remaining)
+    item.tick
+  end
+
+  def backstage_tick
+    @item = Backstage.new(quality, days_remaining)
+    item.tick
+  end
+
+  def quality
+    return item.quality if item
+    @quality
+  end
+
+  def days_remaining
+    return items.days_remaining if item
+    @days_remaining
+  end
+
+
   def tick
     case name
     when "Normal Item"
@@ -17,6 +48,66 @@ class GildedRose
       return sulfuras_tick
     when "Backstage passes to a TAFKAL80ETC concert"
       return backstage_tick
+    end
+
+  class Normal
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
+  end
+
+  class Brie
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
+    end
+  end
+
+  class Sulfuras
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+    end
+  end
+
+  class Backstage
+    attr_reader :quality, :days_remaining
+
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+      return @quality = 0 if @days_remaining < 0
+
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 5
     end
 
     # if name == "Normal Item"
