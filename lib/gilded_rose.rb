72d6eac8e744fda24fb4f5b3f972be462a37d8db
Dante Donaldson <ddonaldsonprime@gmail.com>
2024-02-27 21:33:24 -0600

    Implemented Conjured class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,27 +1,4 @@
 module GildedRose
-  DEFAULT_CLASS = Item
-  SPECIALIZED_CLASSES = {
-    "Normal Item"                                       => Normal,
-    "Aged Brie"                                         => Brie,
-    "Backstage passes to a TAFKAL80ETC concert"         => Backstage  }
-
-  def self.new(name:, days_remaining:, quality:)
-    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
-  end
-
-  def self.klass_for(name)
-    case name
-    when "Normal Item"
-      Normal
-    when "Aged Brie"
-      Brie
-    when "Sulfuras, Hand of Ragnaros"
-      Item
-    when "Backstage passes to a TAFKAL80ETC concert"
-      Backstage
-    end
-  end
-
   class Item
     attr_reader :quality, :days_remaining
 
@@ -64,6 +41,82 @@ module GildedRose
       @quality += 1 if @days_remaining < 5
     end
   end
+
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -= 2 if days_remaining <= 0
+    end
+  end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    "Normal Item"                                       => Normal,
+    "Aged Brie"                                         => Brie,
+    "Backstage passes to a TAFKAL80ETC concert"         => Backstage  }
+
+  def self.new(name:, days_remaining:, quality:)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
+  end
+
+  def self.klass_for(name)
+    case name
+    when "Normal Item"
+      Normal
+    when "Aged Brie"
+      Brie
+    when "Sulfuras, Hand of Ragnaros"
+      Item
+    when "Backstage passes to a TAFKAL80ETC concert"
+      Backstage
+    end
+  end
+
+  # class Item
+  #   attr_reader :quality, :days_remaining
+
+  #   def initialize(quality, days_remaining)
+  #     @quality, @days_remaining = quality, days_remaining
+  #   end
+
+  #   def tick
+  #   end
+  # end
+
+  # class Normal < Item
+  #   def tick
+  #     @days_remaining -= 1
+  #     return if @quality == 0
+
+  #     @quality -= 1
+  #     @quality -= 1 if @days_remaining <= 0
+  #   end
+  # end
+
+  # class Brie < Item
+  #   def tick
+  #     @days_remaining -= 1
+  #     return if @quality >= 50
+
+  #     @quality += 1
+  #     @quality += 1 if @days_remaining <= 0 && @quality < 50
+  #   end
+  # end
+
+  # class Backstage < Item
+  #   def tick
+  #     @days_remaining -= 1
+  #     return if @quality >= 50
+  #     return @quality = 0 if @days_remaining < 0
+
+  #     @quality += 1
+  #     @quality += 1 if @days_remaining < 10
+  #     @quality += 1 if @days_remaining < 5
+  #   end
+  # end
 
   def normal_tick
     @item = Normal.new(quality, days_remaining)
